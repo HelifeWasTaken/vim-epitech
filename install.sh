048c7c4f45e52e7d6ec0229f3eb17c41a48687c2
Mattis DALLEAU <mattis.dalleau@epitech.eu>
2022-10-31 13:25:55 +0100

    Adding system wide installation (Might fail on ubuntu)

--- install.sh
+++ install.sh
@@ -6,14 +6,26 @@
 ## vim configuration installer
 ##
 
-VIM_EPITECH_PATH=/etc/vim/epitech.vim
+VIMRC_DEFAULT_SYSTEM=/etc/vim/vimrc
 VIMRC_SYSTEM_WIDE=/etc/vim/vimrc.local
 VIMRC_LOCAL="/home/$(whoami)/.vimrc"
 
-add_configuration_to_file() {
-    grep -q "source $VIM_EPITECH_PATH" "$1"
+ensure_system_wide_config() {
+	grep "source $VIMRC_SYSTEM_WIDE" "$VIMRC_DEFAULT_SYSTEM"
 
-    if [[ $? != 0 ]]; then
+	if [ $? != 0 ]; then
+		echo "Setup global vimrc"
+		touch "$VIMRC_DEFAULT_SYSTEM"
+		chmod 666 "$VIMRC_DEFAULT_SYSTEM"
+
+		echo "source $VIMRC_SYSTEM_WIDE"  >> $VIMRC_DEFAULT_SYSTEM
+	fi
+}
+
+add_configuration_to_file() {
+    grep "source $VIM_EPITECH_PATH" "$1"
+
+    if [ $? != 0 ]; then
         echo "Epitech configuration path does not exist setting one..."
         cat >> "$1" << EOF
 ""
@@ -35,18 +47,30 @@ EOF
 install() {
     echo "Installing Vim configuration"
 
-    mkdir -p /etc/vim && cp epitech.vim "$VIM_EPITECH_PATH"
+    touch "$1"
+    cp epitech.vim "$VIM_EPITECH_PATH"
 
     add_configuration_to_file "$1"
 
     echo "epitech-vim sucessfully installed"
 }
 
+usage() {
+	echo "To install in local use ./install.sh local"
+	echo
+	echo "To install on the system use ./install.sh system"
+}
+
 case "$1" in
   local )
+	VIM_EPITECH_PATH="$HOME/vim/epitech.vim"
+	mkdir -p "$HOME/vim"
 	install "$VIMRC_LOCAL"
   ;;
   system )
+	mkdir -p /etc/vim
+	ensure_system_wide_config
+	VIM_EPITECH_PATH="/etc/vim/epitech.vim"
 	install "$VIMRC_SYSTEM_WIDE"
   ;;
   * )
